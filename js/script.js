c9af1d2bfacb31aff40b35dca92280378f807b0a
rayannaeem786 <chaudhryrayyan68@gmail.com>
2020-08-16 12:41:40 +0100

    fox

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function myfun(){
+    var a = confirm("To view one more webiste click OK");
+    if (a === true) {
+     window.open("https://rayannaeem786.github.io/estate-agent.github.io");
+    } else {
+        alert("as you wish");
+    };
+ 
+  };
+ 
+ function myfunction() {
+     //  prompt("Welcome to Rayan's Websites. Please give us a feedback");
+     var person = prompt("For verification Please enter your name");
+ 
+     if (person != null) {
+         var apple = prompt("Hello " + person + "! How are you today?");
+         if (apple == "fine") {
+             alert("Good");
+         } else {
+             alert("Okay Bye Thanks for Visiting Our Webiste");
+         };
+ 
+     }
+ }
