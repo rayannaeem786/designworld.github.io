cf629c57a0accce38da0b53fec2b16ecd8e26d41
rayannaeem786 <chaudhryrayyan68@gmail.com>
2020-08-17 13:30:08 +0100

    cvcv

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
-function myfun(){
-    var a = confirm("To view one more webiste click OK");
-    if (a === true) {
-     window.open("https://rayannaeem786.github.io/estate-agent.github.io");
-    } else {
-        alert("as you wish");
-    };
+// function myfun(){
+//     var a = confirm("To view one more webiste click OK");
+//     if (a === true) {
+//      window.open("https://rayannaeem786.github.io/estate-agent.github.io");
+//     } else {
+//         alert("as you wish");
+//     };
  
-  };
+//   };
  
- function myfunction() {
-     //  prompt("Welcome to Rayan's Websites. Please give us a feedback");
-     var person = prompt("For verification Please enter your name");
+//  function myfunction() {
+//      //  prompt("Welcome to Rayan's Websites. Please give us a feedback");
+//      var person = prompt("For verification Please enter your name");
  
-     if (person != null) {
-         var apple = prompt("Hello " + person + "! How are you today?");
-         if (apple == "fine") {
-             alert("Mobile screen recording enabled 😜");
-         } else {
-            alert("Mobile screen recording enabled 😜");
-        };
+//      if (person != null) {
+//          var apple = prompt("Hello " + person + "! How are you today?");
+//          if (apple == "fine") {
+//              alert("Mobile screen  recording enabled 😜");
+//          } else {
+//             alert("Mobile screen recording enabled enabled 😜");
+//         };
  
-     }
- }
+//      }
+//  }
+$(document).ready(function(){
+    $("#exampleModal").modal('show');
+});
+
+
+function myFunction(){
+    document.getElementById("java-btn").style.display = "block";
+};
