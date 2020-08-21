75039c9530590401dda1e3c6055c23975a70f24c
rayannaeem786 <chaudhryrayyan68@gmail.com>
2020-08-21 12:38:49 +0100

    works

--- js/script.js
+++ js/script.js
@@ -1,30 +1,13 @@
 $(document).ready(function () {
     $("#exampleModal").modal('show');
 });
+
 var games = document.getElementById("myId");
 var texting = document.getElementById("gameText");
 function mainFuny() {
     var apple = document.getElementById("main-id").value;
-    if (apple == "rayan") {
-
-    }else if(apple == "Rayan") {
-
-
-    }
-    else if(apple == "abdullah") {
-
-
-    }
-    else if(apple == "Abdullah") {
-
-
-    }
-    else if(apple == "Aqeel") {
-
-
-    }
-    else if(apple == "aqeel") {
-
+    if (apple !== "") {
+    texting.innerHTML = "Hi" + "&nbsp;" + apple + "!" + "&nbsp;" + "Thanks for visiting our website";
 
     }
      else{
